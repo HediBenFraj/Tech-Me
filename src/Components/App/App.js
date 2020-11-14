d867d882b948d624578fd6035b85e7a58e93159c
HediBenFraj <benfraj1998@gmail.com>
2020-11-14 02:28:51 +0100

    second commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import {Navbar } from '../Navbar'
 import {NewsSection} from '../NewsSection'
-import {CreateNews} from '../CreateNewsSection'
+
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import './App.css'
 
@@ -11,15 +11,7 @@ function App(){
     return(
         <div className="app">
             <Navbar/>
-            <Router>
-                <Switch>
-                <Route path="/create" component={CreateNews}/>
-                <Route path="/view" component={NewsSection}/>
-                </Switch>
-            </Router>
-       
-          
-            
+            <NewsSection/>
         </div>
     )
 }
